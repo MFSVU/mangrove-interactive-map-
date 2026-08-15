fc4a4b7f2263cd18501796df01e9329ba12831a2
MFSVU <mohamedfawzy@eng.svu.edu.eg>
2026-08-15 23:33:34 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,9 @@
 // Years of analysis
 var YEARS = [2016, 2019, 2022, 2025];
 
+// AOI Names
+var AOI_NAMES = ['AOI_025'];
+
 // Base map options
 var BASEMAPS = {
     'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,31 +21,30 @@ var BASEMAPS = {
 // NDVI Color Palette
 var NDVI_PALETTE = {
     min: -0.5,
-    max: 1.0,
-    colors: [
-        '#d73027', // -0.5 - 0.0
-        '#f46d43', // 0.0 - 0.2
-        '#fdae61', // 0.2 - 0.4
-        '#fee08b', // 0.4 - 0.6
-        '#d9ef8b', // 0.6 - 0.8
-        '#a6d96a'  // 0.8 - 1.0
-    ]
+    max: 0.8,
+    colors: ['#d73027', '#f46d43', '#fdae61', '#fee08b', '#d9ef8b', '#a6d96a']
 };
 
-// Data file paths - use PNG for simplicity (convert from TIFF)
+// Data file paths - UPDATED for your files
 var DATA_PATHS = {
-    aois: 'data/AOI_001_geojson.geojson',
+    aois: 'data/AOIs_Selected_Geojson.geojson',
     mangroveMasks: {
-        2016: 'data/mangrove_masks/AOI_001_mask_2016.png',
-        2019: 'data/mangrove_masks/AOI_001_mask_2019.png',
-        2022: 'data/mangrove_masks/AOI_001_mask_2022.png',
-        2025: 'data/mangrove_masks/AOI_001_mask_2025.png'
+        2016: 'data/mangrove_masks/AOI_025_mask_2016.tif',
+        2019: 'data/mangrove_masks/AOI_025_mask_2019.tif',
+        2022: 'data/mangrove_masks/AOI_025_mask_2022.tif',
+        2025: 'data/mangrove_masks/AOI_025_mask_2025.tif'
     },
     ndviRasters: {
-        2016: 'data/ndvi_rasters/AOI_001_ndvi_2016.png',
-        2019: 'data/ndvi_rasters/AOI_001_ndvi_2019.png',
-        2022: 'data/ndvi_rasters/AOI_001_ndvi_2022.png',
-        2025: 'data/ndvi_rasters/AOI_001_ndvi_2025.png'
+        2016: 'data/ndvi_rasters/AOI_025_ndvi_2016.tif',
+        2019: 'data/ndvi_rasters/AOI_025_ndvi_2019.tif',
+        2022: 'data/ndvi_rasters/AOI_025_ndvi_2022.tif',
+        2025: 'data/ndvi_rasters/AOI_025_ndvi_2025.tif'
+    },
+    ndviRaw: {
+        2016: 'data/ndvi_rasters/AOI_025_ndvi_raw_2016.tif',
+        2019: 'data/ndvi_rasters/AOI_025_ndvi_raw_2019.tif',
+        2022: 'data/ndvi_rasters/AOI_025_ndvi_raw_2022.tif',
+        2025: 'data/ndvi_rasters/AOI_025_ndvi_raw_2025.tif'
     }
 };
 
